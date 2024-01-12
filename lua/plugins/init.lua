606cf03a71215e80b0693f5dfb109258d4b97762
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2024-01-11 20:05:35 -0800

    Initial commit
    Setup NVChad

---
+++ lua/plugins/init.lua
@@ -0,0 +1,55 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "clangd",
+        "clang-format",
+        "ruff-lsp",
+        "pyright",
+        "black",
+        "java_language_server",
+        "bash-language-server",
+        "shfmt",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "prettier",
+        "eslint-lsp",
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+      },
+    },
+  },
+}
