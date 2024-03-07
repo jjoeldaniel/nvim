604a7ad4c2853385bea0ce635de7e91c2df4c52e
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2024-03-07 14:42:47 -0800

    move keymaps around

--- lua/plugins/keymaps.lua
+++ lua/plugins/keymaps.lua
@@ -1,1 +1,12 @@
+-- Misc
+vim.keymap.set("n", "<leader>r", ":SearchReplaceSingleBufferOpen<CR>", { silent = true })
+vim.keymap.set("n", "<leader>s", ":SymbolsOutline<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fm", ":LazyFormat<CR>", { silent = true })
+vim.keymap.set("n", "<leader>a", ":AerialToggle<CR>", { silent = true })
+
+-- Spider
+vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
+vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
+
 return {}
