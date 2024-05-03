eb4e06f0b31190e1d2c3e34b9ca8e6e4c7e34d71
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2024-05-03 15:05:33 -0700

    switch to nvchad
    add
    
    fix mason installs

---
+++ lua/plugins/init.lua
@@ -0,0 +1,56 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "clangd",
+        "clang-format",
+        "ruff",
+        "ruff-lsp",
+        "pyright",
+        "black",
+        "java-language-server",
+        "bash-language-server",
+        "shfmt",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "prettier",
+        "eslint-lsp",
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+      },
+    },
+  },
+}
