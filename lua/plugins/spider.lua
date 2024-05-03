e68acadf11cc3bab867231f12173d8e6f0156022
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2024-05-03 15:21:48 -0700

    add some shit
    enable nvdash on startup
    add leap + spider
    switch file tree to toggle

---
+++ lua/plugins/spider.lua
@@ -0,0 +1,30 @@
+vim.keymap.set(
+	{ "n", "o", "x" },
+	"w",
+	"<cmd>lua require('spider').motion('w')<CR>",
+	{ desc = "Spider-w" }
+)
+vim.keymap.set(
+	{ "n", "o", "x" },
+	"e",
+	"<cmd>lua require('spider').motion('e')<CR>",
+	{ desc = "Spider-e" }
+)
+vim.keymap.set(
+	{ "n", "o", "x" },
+	"b",
+	"<cmd>lua require('spider').motion('b')<CR>",
+	{ desc = "Spider-b" }
+)
+
+-- lazy.nvim
+return {
+	"chrisgrieser/nvim-spider",
+	keys = {
+		{
+			"e",
+			"<cmd>lua require('spider').motion('e')<CR>",
+			mode = { "n", "o", "x" },
+		},
+	},
+}
