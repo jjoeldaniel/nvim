0f217e6b7f6d5fcba71b2d0505e6a3ac9d678dbb
Joel Daniel Rico <joeldanielrico@gmail.com>
2024-05-02 23:43:44 -0700

    add lazy fix to multiple encodings

--- init.lua
+++ init.lua
@@ -17,3 +17,9 @@ end
 
 require "lazy_setup"
 require "polish"
+
+local notify = vim.notify
+vim.notify = function(msg, ...)
+  if msg:match "warning: multiple different client offset_encodings" then return end
+  notify(msg, ...)
+end
