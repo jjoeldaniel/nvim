8ee499652a1073cc44537a94e42d04e27489f54a
Joel Daniel Rico <joeldanielrico@gmail.com>
2024-06-04 21:57:05 -0700

    osc52 timeout bandaid fix

--- lua/options.lua
+++ lua/options.lua
@@ -4,14 +4,31 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
-vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy "+",
-    ["*"] = require("vim.ui.clipboard.osc52").copy "*",
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste "+",
-    ["*"] = require("vim.ui.clipboard.osc52").paste "*",
-  },
+
+-- OSC52 fuckery
+local opt = vim.opt
+function my_paste(reg)
+    return function(lines)
+        local content = vim.fn.getreg('"')
+        return vim.split(content, '\n')
+        
+    end
+end
+
+if (os.getenv('SSH_TTY') == nil)
+then
+    opt.clipboard:append("unnamedplus")
+else
+    opt.clipboard:append("unnamedplus")
+    vim.g.clipboard = {
+      name = 'OSC 52',
+      copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+      },
+      paste = {
+        ["+"] = my_paste("+"),
+        ["*"] = my_paste("*"),
+    },
 }
+end
